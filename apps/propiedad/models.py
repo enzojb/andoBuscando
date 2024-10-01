5c44997ce2560a6d8b1709a9ec7d19281c9b698a
NicoRepa <nicolasreparaz@gmail.com>
2024-10-01 19:43:50 -0300

    cambio de nombre en models.py de la app tipo_propiedad

--- apps/propiedad/models.py
+++ apps/propiedad/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from apps.localidad.models import Localidad
-from apps.tipo_propiedad.models import Tipo_propiedad
-from apps.propiedad_foto.models import Propiedad_foto
+from apps.tipo_propiedad.models import Tipo_Propiedad
 from apps.agente.models import Agente
 
 class Propiedad(models.Model):
     id = models.AutoField(primary_key=True)
     localidad = models.OneToOneField(Localidad, on_delete=models.CASCADE)
     agente = models.OneToOneField(Agente, on_delete=models.CASCADE)
-    tipo_propiedad = models.OneToOneField(Tipo_propiedad, on_delete=models.CASCADE)
+    tipo_propiedad = models.OneToOneField(Tipo_Propiedad, on_delete=models.CASCADE)
     ambientes = models.CharField(max_length=2)
     dormitorios = models.CharField(max_length=2)
     banios = models.CharField(max_length=2)
--- apps/tipo_propiedad/models.py
+++ apps/tipo_propiedad/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class TipoPropiedad(models.Model):
+class Tipo_Propiedad(models.Model):
     id = models.AutoField(primary_key=True)
     tipo_propiedad = models.CharField(max_length=100)
